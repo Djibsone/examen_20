b59f78cd5aaf40908007fc3a4ca39134e86ec002
Djibril <djibriltamou@gmail.com>
2023-07-22 01:51:41 +0100

    init

---
+++ controllers/affiche.php
@@ -0,0 +1,9 @@
+<?php
+require_once('./models/connexion.php');
+
+    $projets = getAllProjets();
+    // $stmt = $projets->fetch(PDO::FETCH_ASSOC);
+    // var_dump($stmt);
+    
+    //compter le numbre candidats par filere
+    $total_projets = countProjets(); 
